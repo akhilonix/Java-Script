6c267f558945b007c0d51eca152805e165162e84
akhilonix <akhileshgangwari.p.s79@gmail.com>
2026-09-17 11:27:10 +0530

    Update JavaScript project

--- Day1.js
+++ Day1.js
@@ -1,8 +1,31 @@
-let a =10;
-console.log(a);
+let x =10;
+x=40;
+console.log(x);
+console.log(x);
 let arr = [1, 2, 3];
 console.log(arr);
 let obj = { name: "John", age: 30 };
 console.log(obj);
 console.warn("This is a warning message");
 console.error("This is an error message");
+
+//var
+//var a=10;
+//var a=20;
+//console.log(a);
+//console.log(a);
+
+//const
+//const b=30; 
+// b=40 error
+//const b=50; error
+//console.log(b);
+
+
+
+var d=50;
+if(true){
+    let c=60;
+    let y=100;
+    console.log(c,y,d);
+}
