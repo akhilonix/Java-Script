dacf2e56e17f86c8311950f9da5499bff40afd4c
akhilonix <akhileshgangwari.p.s79@gmail.com>
2026-09-17 10:07:40 +0530

    Update JavaScript files

---
+++ Day1.js
@@ -0,0 +1,8 @@
+let a =10;
+console.log(a);
+let arr = [1, 2, 3];
+console.log(arr);
+let obj = { name: "John", age: 30 };
+console.log(obj);
+console.warn("This is a warning message");
+console.error("This is an error message");
